6a86a015d5f368bc79588f8a1d7ffbab8008b05b
Varun Balaji <varunbalaji06@gmail.com>
2026-01-23 15:11:55 +0530

    Fix dataset path for GitHub Actions CI

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, f1_score
 
-DATA_PATH = "dataset/winequality-red.csv"
+DATA_PATH = "Lab_4/dataset/winequality-red.csv"
 
 data = pd.read_csv(DATA_PATH, sep=";")
 
